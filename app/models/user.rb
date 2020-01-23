5ca7ec37a7362fb7083a9c1840c15d68420758cf
Jun Takamatsu <jtakamatsu@pasonatquila.com>
2020-01-23 18:47:56 +0900

    done chapter8

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  name            :string
+#  email           :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string
+#
+
 class User < ApplicationRecord
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
@@ -7,4 +19,10 @@ class User < ApplicationRecord
   uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+               BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
